e4a41110f3a97fbfd13fe981867e36010db5a161
Nawreskhalifa <nawreskhalifa4tech1@gmail.com>
2026-03-30 23:24:22 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,23 @@
-FROM node:18
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-EXPOSE 3000
-CMD ["npm", "run", "develop"]
+# Image officielle Node.js
+FROM node:20
 
+# Dossier de travail dans le container
+WORKDIR /app
+
+# Copier package.json et package-lock.json
+COPY package*.json ./
+
+# Installer les dépendances
+RUN npm install
+
+# Copier le reste du projet
+COPY . .
+
+# Build Strapi (si nécessaire)
+RUN npm run build
+
+# Exposer le port (Strapi = 1337)
+EXPOSE 3000
+
+# Commande pour lancer l'app
+CMD ["npm", "run", "develop"]
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,31 @@
 module.exports = [
-  "strapi::logger",
   "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-  "global::role-middleware",
+
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": [
+            "'self'",
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://analytics.strapi.io"
+          ],
+          "img-src": ["'self'", "data:", "blob:", "http:"],
+          "media-src": ["'self'", "data:", "blob:", "http:"],
+          "script-src": ["'self'", "'unsafe-inline'"],
+        },
+      },
+    },
+  },
+
   {
     name: "strapi::cors",
     config: {
       enabled: true,
+      contentSecurityPolicy: false,
       headers: [
         "Access-Control-Allow-Headers",
         "withCredentials",
@@ -25,7 +37,14 @@ module.exports = [
         "Access-Control-Request-Method",
         "Access-Control-Request-Headers",
       ],
-      origin: ["http://localhost:8080", "http://localhost:1337",'http://localhost:3000','http://localhost:8081',]
+      origin: ["http://localhost:8080", "http://localhost:1337",'http://localhost:3000','http://localhost:8081', 'http://localhost:5173',"http://localhost:3001","http://localhost", "http://localhost:80"]
+    
     },
   },
+
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
